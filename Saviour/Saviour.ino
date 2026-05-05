f5851fc2c0f74c5302dd49e918ab4a79d6aa9f6f
Resham <iamresham.s@gmail.com>
2026-05-05 12:10:51 +0100

    Update Saviour.ino

--- Saviour/Saviour.ino
+++ Saviour/Saviour.ino
@@ -1,6 +1,7 @@
 // Define the pins we are using
 const int buttonPin = 2;    // The digital pin connected to the button
 const int greenLEDPin = 3;  // The digital pin connected to the Green leg of the RGB LED
+const int redLEDPin = 4;    // The digital pin connected to the Red leg of the RGB LED
 
 // Variable to store the current state of the button
 int buttonState = 0; 
@@ -9,12 +10,11 @@ void setup() {
   // Initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   
-  // Set the green LED pin as an output
+  // Set the LED pins as outputs
   pinMode(greenLEDPin, OUTPUT);
+  pinMode(redLEDPin, OUTPUT);
   
-  // Set the button pin as an input.
-  // INPUT_PULLUP turns on the Arduino's internal resistor, 
-  // pulling the pin HIGH (3.3V) when the button is NOT pressed.
+  // Set the button pin as an input with the internal pull-up resistor
   pinMode(buttonPin, INPUT_PULLUP);
 }
 
@@ -24,16 +24,20 @@ void loop() {
 
   // Because we used INPUT_PULLUP, the button reads LOW when pressed.
   if (buttonState == LOW) {
-    // Button is pressed: turn the Green LED ON
+    // BUTTON IS PRESSED:
+    // Turn the Green LED ON and the Red LED OFF
     digitalWrite(greenLEDPin, HIGH);
+    digitalWrite(redLEDPin, LOW);
     
     // Print to the Serial Monitor
-    Serial.println("Button is PRESSED!");
+    Serial.println("Button is PRESSED! Light is GREEN.");
     
     // A small delay prevents the serial monitor from being flooded with text instantly
     delay(100); 
   } else {
-    // Button is not pressed: turn the Green LED OFF
+    // BUTTON IS NOT PRESSED: 
+    // Turn the Red LED ON and the Green LED OFF
+    digitalWrite(redLEDPin, HIGH);
     digitalWrite(greenLEDPin, LOW);
   }
 }
