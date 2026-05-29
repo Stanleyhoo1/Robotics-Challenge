a2a22958942065df1729d62431176b1fd698513e
Resham Srivastava <63190230+DarkerShadeofDolphin@users.noreply.github.com>
2026-05-29 12:46:50 +0100

    Clean up comments in Saviour.ino

--- Saviour/Saviour.ino
+++ Saviour/Saviour.ino
@@ -1,4 +1,3 @@
-// Define the pins we are using
 const int buttonPin1 = 2;    // The digital pin connected to the first button
 const int buttonPin2 = 5;    // The digital pin connected to the second button
 const int greenLEDPin = 49;   // The digital pin connected to the Green leg of the RGB LED
@@ -12,30 +11,23 @@ void setup() {
   // Initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   
-  // Set the LED pins as outputs
   pinMode(greenLEDPin, OUTPUT);
   pinMode(redLEDPin, OUTPUT);
   
-  // Set BOTH button pins as inputs with the internal pull-up resistor
   pinMode(buttonPin1, INPUT_PULLUP);
   pinMode(buttonPin2, INPUT_PULLUP);
 }
 
 void loop() {
-  // Read the state of BOTH pushbuttons:
   buttonState1 = digitalRead(buttonPin1);
   buttonState2 = digitalRead(buttonPin2);
 
-  // Because we used INPUT_PULLUP, the buttons read LOW when pressed.
-  // The "||" symbol means "OR". This statement reads: 
-  // "If button 1 is LOW -OR- button 2 is LOW..."
   if (buttonState1 == LOW || buttonState2 == LOW) {
     // AT LEAST ONE BUTTON IS PRESSED:
     // Turn the Green LED ON and the Red LED OFF
     digitalWrite(greenLEDPin, HIGH);
     digitalWrite(redLEDPin, LOW);
     
-    // Print to the Serial Monitor
     Serial.println("A button is PRESSED! Light is GREEN.");
     
     // A small delay prevents the serial monitor from being flooded with text instantly
